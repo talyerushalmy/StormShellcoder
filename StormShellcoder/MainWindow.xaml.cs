7f690e95ae9f12b9c1a56268572460f971f3d067
Tal Yerushalmi <tal.y3rushalmi@gmail.com>
2018-01-11 03:34:51 +0200

    Working prototype

--- StormShellcoder/MainWindow.xaml.cs
+++ StormShellcoder/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace StormShellcoder
 {
@@ -25,9 +15,58 @@ namespace StormShellcoder
             InitializeComponent();
         }
 
+        private string getBinDirectoryPath()
+        {
+            Assembly ass = Assembly.GetExecutingAssembly();
+            return System.IO.Path.GetDirectoryName(ass.Location);
+        }
+
+        private System.Diagnostics.Process createProcess()
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.WorkingDirectory = getBinDirectoryPath();
+            startInfo.FileName = "nasm";
+            startInfo.Arguments = "-o temp/output temp/input_code.asm";
+            process.StartInfo = startInfo;
+
+            return process;
+        }
+
         private void buttonAssemble_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                string userInput = this.textBoxInput.Text;
 
+                FileInfo file = new FileInfo("temp/input_code.asm");
+                file.Directory.Create();
+                File.WriteAllText(file.FullName, userInput);
+
+                System.Diagnostics.Process process = createProcess();
+                process.Start();
+                process.WaitForExit();
+
+                try
+                {
+                    Byte[] output = File.ReadAllBytes("temp/output");
+                    this.textBoxOutput.Text = BitConverter.ToString(output);
+                }
+                catch
+                {
+                    this.textBoxOutput.Text = "Assembler raised an error.\nPlease check your code.";
+                }
+            }
+            catch (Exception exception)
+            {
+                this.textBoxOutput.Text = exception.ToString();
+            }
+        }
+
+        private void buttonCopy_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(this.textBoxOutput.Text);
         }
     }
 }
